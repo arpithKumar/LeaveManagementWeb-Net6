79a2e6e38bfabc500dcd6a6ae1fc4b0a560760ab
Arpith kumar <arpithk3@outlook.com>
2022-12-30 10:28:32 +0530

    Added Migration Tables

---
+++ LeaveManagement.Web/Data/Employee.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeaveManagement.Web.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string TxId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateOfJoining { get; set; }
+
+    }
+}
